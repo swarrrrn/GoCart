5c0d97e041dfd6078d80a29ca02204f9e6e6e59e
swarnkumar.101 <swarnkumar.101@gmail.com>
2026-07-03 22:55:58 +0530

    category and image service implementation completed

---
+++ src/main/java/com/swkmspringcart/GoCart/dto/ImageDto.java
@@ -0,0 +1,10 @@
+package com.swkmspringcart.GoCart.dto;
+
+import lombok.Data;
+
+@Data
+public class ImageDto {
+    private Long imageId;
+    private String imageName;
+    private String downloadUrl;
+}
