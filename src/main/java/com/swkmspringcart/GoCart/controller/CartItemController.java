07ae5625d3c4bdd875b96314291b0cf9a9db6b8c
swarnkumar.101 <swarnkumar.101@gmail.com>
2026-07-19 15:51:06 +0530

    feat: integrated jwt for users and admins

--- src/main/java/com/swkmspringcart/GoCart/controller/CartItemController.java
+++ src/main/java/com/swkmspringcart/GoCart/controller/CartItemController.java
@@ -7,11 +7,13 @@ import com.swkmspringcart.GoCart.response.ApiResponse;
 import com.swkmspringcart.GoCart.service.cart.ICartItemService;
 import com.swkmspringcart.GoCart.service.cart.ICartService;
 import com.swkmspringcart.GoCart.service.user.IUserService;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,13 +29,15 @@ public class CartItemController {
             @RequestParam Long productId,
             @RequestParam Integer quantity) {
         try {
-            User user = userService.getUserById(4L);
+            User user = userService.getAuthenticatedUser();
             Cart cart= cartService.initializeNewCart(user);
 
             cartItemService.addItemToCart(cart.getId(), productId, quantity);
             return ResponseEntity.ok(new ApiResponse("Add Item Success", null));
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }catch (JwtException e){
+            return  ResponseEntity.status(UNAUTHORIZED).body(new ApiResponse(e.getMessage(), null));
         }
     }
 
